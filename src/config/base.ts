48f216d2ab73741ac3f15bd079751ab5ff08ec35
Michael Thiesen <michael@understorytech.com>
2024-10-26 15:42:17 -0700

    Bumped version to 1.1.1 and updated config schema validation

--- src/config/base.ts
+++ src/config/base.ts
@@ -1,11 +1,8 @@
 import { z } from "zod";
 import { LogLevel } from "../logging/types.js";
 
-const stringInt = z.string().transform((val) => Number.parseInt(val, 10));
 export const baseConfigSchema = z.object({
-  PORT: stringInt,
   LOG_LEVEL: z.nativeEnum(LogLevel).optional().default(LogLevel.INFO),
-  AUTH_TOKEN: z.string(),
   PUSHOVER_USER: z.string(),
   PUSHOVER_TOKEN: z.string(),
 });
