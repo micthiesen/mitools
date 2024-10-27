ef92b8209d6ed9287899fe8be0ae13b421f88a9e
Michael Thiesen <michael@understorytech.com>
2024-10-27 16:16:54 -0700

    Bump version and enhance entity logging functionality

--- src/config/base.ts
+++ src/config/base.ts
@@ -7,7 +7,7 @@ export const baseConfigSchema = z.object({
   PUSHOVER_USER: z.string(),
   PUSHOVER_TOKEN: z.string(),
   DOCKERIZED: z.string().optional().default("false").transform(stringBoolean),
-  DB_NAME: z.string().optional().default("docstore"),
+  DB_NAME: z.string().optional().default("docstore.db"),
 });
 
 const PRIVATE_CONFIG_KEYS: (string & keyof BaseConfig)[] = [
--- src/persistence/entities.ts
+++ src/persistence/entities.ts
@@ -1,4 +1,8 @@
+import { Logger } from "../logging/Logger.js";
+import { kebabToTitleCase } from "../utils/strings.js";
 import { getDoc, upsertDoc } from "./docstore.js";
+
+const logger = new Logger("Entities");
 
 /**
  * A class representing an entity in the database
@@ -9,11 +13,13 @@ import { getDoc, upsertDoc } from "./docstore.js";
  *   - It does not support any kind of query besides getting by primary key
  */
 export class Entity<Data, PKProps extends readonly (keyof Data)[]> {
+  private readonly logger: Logger;
   public readonly name: string;
   public readonly pkProps: PKProps;
   public readonly getPk: (arg: Pick<Data, PKProps[number]>) => string;
 
   public constructor(name: string, pkProps: PKProps) {
+    this.logger = logger.extend(kebabToTitleCase(name));
     this.name = name;
     this.pkProps = pkProps;
     this.getPk = (arg: Pick<Data, PKProps[number]>) =>
@@ -21,10 +27,17 @@ export class Entity<Data, PKProps extends readonly (keyof Data)[]> {
   }
 
   public get(arg: Pick<Data, PKProps[number]>): Data | undefined {
-    return getDoc(this.getPk(arg));
+    const doc = getDoc<Data>(this.getPk(arg));
+    if (doc) {
+      this.logger.debug(`Found "${this.getPk(arg)}" in docstore`, doc);
+    } else {
+      this.logger.debug(`"${this.getPk(arg)}" not found in docstore`);
+    }
+    return doc;
   }
 
   public upsert(data: Data): void {
     upsertDoc(this.getPk(data), data);
+    this.logger.debug(`Upserted "${this.getPk(data)}" in docstore`, data);
   }
 }
